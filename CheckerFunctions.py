434581f15088232c33cc6bd20ccceaffa7d40e93
Kataklizmm <instigator872@mail.ru>
2020-07-13 21:50:43 +0300

    Add files via upload

---
+++ CheckerFunctions.py
@@ -0,0 +1,78 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import os
+
+
+if os.path.isfile('data.json'):
+    with open('data.json', 'r') as f:
+        storage = json.load(f)
+else:
+    f = open('data.json', 'w')
+    f.close()
+    storage = {}
+
+# Функция получает ответ сервера и метатеги
+def get_url_data(url):
+    # Пробуем получить ответ, если 200 - собираем нужные данные, если нет, записываем URL в список "плохих"
+    try:
+        bad_response = None
+        r = requests.get(url, allow_redirects=False)
+        rawhtml = BeautifulSoup(r.text, 'lxml')  # Здесь голая HTML
+        # Собираем нужные данные
+        response = str(r.status_code)
+        if response != '200':
+            bad_responses.append(str(url.strip()) + '\t' + str(response))
+
+        title = rawhtml.title
+        if title:
+            title = title.text
+
+        h1 = rawhtml.h1
+        if h1:
+            h1 = h1.text
+
+        canonical = rawhtml.head.find('link', attrs={'rel': 'canonical'})
+        if canonical:
+            canonical = canonical['href']
+
+        description = rawhtml.head.find('meta', attrs={'name': 'description'})
+        if description:
+            description = description['content']
+
+        meta_robots = rawhtml.head.select('[name=robots], [name=googlebot], [name=yandex]')
+        if meta_robots:
+            meta_robots = [i.attrs for i in meta_robots]
+        else:
+            meta_robots = None
+
+        try: x_robots = r.headers['X-Robots-Tag']
+        except Exception: x_robots = None
+
+        # Сохраняем данные в виде словаря
+        new_data = {'response': response, 'title': title, 'h1': h1,
+                    'description': description, 'canonical': canonical, 'meta_robots': meta_robots, 'x_robots_tag': x_robots}
+        return new_data
+    except:
+        bad_response = str(url) + ' - Нет ответа'
+        return bad_response
+
+# Функция сравнивает данные каждого URL между записанными в БД,
+# если есть разница, записывает новые данные, разницу скидывает в файл
+def changes_checker(new_data, url):
+    changes = []
+    if url not in storage.keys():
+        storage[url] = new_data
+    else:
+        if storage[url] != new_data:
+            changes.append("Есть изменения на странице: " + str(url))
+            for key, value in storage[url].items():
+                if value != new_data[key]:
+                    changes.append('Старый {}: {}'.format(key, value))
+                    changes.append('Новый {}: {}\n'.format(key, new_data[key]))
+            storage[url] = new_data
+
+    with open('data.json', 'w') as f:
+        json.dump(storage, f)
+
+    return changes
---
+++ check.py
@@ -0,0 +1,31 @@
+import time
+import CheckerFunctions as CF
+
+# Взять урлы из файла urls и проверить каждый. В конце открыть блокнот со списком ошибок если они имеются
+if __name__ == '__main__':
+    urls = open('urls.txt', 'r')
+    changes = []
+    bad_responses = []
+
+    for url in urls:
+        url = url.strip()
+        new_data = CF.get_url_data(url)
+        if type(new_data) == dict:
+            changes.extend(CF.changes_checker(new_data, url))
+        else:
+            bad_responses.append(new_data)
+
+        time.sleep(0.2)
+
+    if len(changes) > 0 or len(bad_responses) > 0:
+        errors = open('errors.txt', 'w', encoding='utf-8')
+        for change in changes:
+            errors.write(change + '\n')
+        if bad_responses:
+            errors.write('Страницы не 200: \n')
+            for i in bad_responses:
+                errors.write(i + '\n')
+        errors.close()
+        print('Есть изменения, смотри errors.txt')
+    else:
+        print('Проверка закончена. Изменений не обнаружено')
